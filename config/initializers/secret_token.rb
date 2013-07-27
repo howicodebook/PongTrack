6eeaa371f654d7c5cc0530787f11d79bc2f6d061
J. Nunn <jbnunn@gmail.com>
2013-07-27 11:25:27 -0500

    Initial push before we start the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pongtracker::Application.config.secret_token = '711770ddaba53e9ce1f0590dd033dbbf6aba3cf5078c401768349fe6ead666d826f648e7433306e9d86091e2519d1f9d1d1088098c033ed4c21d6596c4c6bcff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pongtracker::Application.config.session_store :cookie_store, key: '_pongtracker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pongtracker::Application.config.session_store :active_record_store
